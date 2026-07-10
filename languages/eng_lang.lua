dd8d79b00e243f2b35079f6e12fc65d511fb7d58
jxribeirocodes <jxribeirogithub@outlook.com>
2026-07-10 10:08:11 -0300

    Add English language localization for radius viewer

---
+++ languages/eng_lang.lua
@@ -0,0 +1,8 @@
+---------------------------------- ENGLISH -------------------------------------------
+Locales["en"] = {
+    title = 'Radius Viewer',
+    no_permission = 'You don\'t have permission to use this command.',
+    radius_disabled = 'Radius display disabled.',
+    radius_usage = 'Use /%s 1-%s.',
+    radius_active = 'Viewing radius of %.1fm.',
+}
