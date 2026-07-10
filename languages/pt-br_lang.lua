a817ec7b9a437620c606fc5e97784ee29fec593b
jxribeirocodes <jxribeirogithub@outlook.com>
2026-07-10 10:08:40 -0300

    Add Portuguese (Brazil) localization for Radius Viewer

---
+++ languages/pt-br_lang.lua
@@ -0,0 +1,8 @@
+---------------------------------- PORTUGUÊS (BRASIL) -------------------------------------------
+Locales["pt-br"] = {
+    title = 'Radius Viewer',
+    no_permission = 'Sem permissão para usar este comando.',
+    radius_disabled = 'Visualização de raio desativada.',
+    radius_usage = 'Use /%s 1-%s.',
+    radius_active = 'Visualizando raio de %.1fm.',
+}
